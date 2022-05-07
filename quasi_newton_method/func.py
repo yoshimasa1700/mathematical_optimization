196e5d39313d6ba356510dc1be083493a01370e5
Masahiko Toyoshi <Masahiko.Toyoshi@sony.com>
2022-05-07 17:59:46 +0900

    update func.

--- quasi_newton_method/func.py
+++ quasi_newton_method/func.py
@@ -1,9 +1,24 @@
+import numbers
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+def is_scalar(x):
+    return isinstance(x, numbers.Number)
+
+
 def func(X, Y):
     return (X - 2)**4 + (X - 2*Y)**2
+
+
+def deriv_1st_func(X, Y):
+    delta_x = 4 * (X - 2)**3 + 2 * (X - 2*Y)
+    delta_y = -4 * (X - 2*Y)
+
+    if is_scalar(delta_x):
+        return np.array([[delta_x, delta_y]]).T
+
+    return np.concatenate([delta_x, delta_y], 0)
 
 
 def main():
